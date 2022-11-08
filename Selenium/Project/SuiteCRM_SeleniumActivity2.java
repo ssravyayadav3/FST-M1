f99f9c7efe69623f3eb662b153cb59aa1d9b0dbb
ssravyayadav3 <116861369+ssravyayadav3@users.noreply.github.com>
2022-11-08 14:37:09 +0530

    Add files via upload

---
+++ Selenium/Project/SuiteCRM_SeleniumActivity2.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SuiteCRM_SeleniumActivity2 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://alchemy.hguy.co/crm");
+        WebElement URL = driver.findElement(By.xpath("//img[@alt='SuiteCRM']"));
+        System.out.println("URL of the Source image is: "+URL.getAttribute("src"));
+        driver.close();
+    }
+}
---
+++ Selenium/Project/SuiteCRM_SeleniumActivity3.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SuiteCRM_SeleniumActivity3 {
+    public static void main(String[] args) {
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("http://alchemy.hguy.co/crm");
+        String copyRightText = driver.findElement(By.xpath("//a[@id='admin_options']")).getText();
+        System.out.println(copyRightText);
+        driver.close();
+    }
+
+}
