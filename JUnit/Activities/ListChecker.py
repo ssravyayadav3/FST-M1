7901f60ff18a516f2cb0792f9771d201ea97c4a2
ssravyayadav3 <116861369+ssravyayadav3@users.noreply.github.com>
2022-11-14 09:10:32 +0530

    Add files via upload

---
+++ JUnit/Activities/ListChecker.py
@@ -0,0 +1,8 @@
+myList = list(input("Enter list values: "))
+print(myList[0])
+print(myList[-1])
+print(myList)
+FNum = myList[0]
+LNum = myList[-1]
+if (FNum == LNum):
+    print("First and Last numbers of the list are same") 
---
+++ JUnit/Activities/OddOrEven.py
@@ -0,0 +1,6 @@
+number = int(input("Enter numbers: "))
+formulae = number%2
+if formulae>0:
+    print("Number is Odd")
+else:
+    print("Number is even")
