ff7c69b716cb149f4cb17b25d084792c14e7ae45
ssravyayadav3 <116861369+ssravyayadav3@users.noreply.github.com>
2022-11-30 17:19:46 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity7.java
@@ -0,0 +1,57 @@
+package orgexample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class Activity7 {
+        WebDriver driver;
+        WebDriverWait wait;
+
+        @BeforeClass
+        public void beforeClass() {
+            driver = new FirefoxDriver();
+            //Open browser
+            driver.get("https://www.training-support.net/selenium/login-form");
+        }
+
+    public WebDriverWait(WebDriver driver, Duration timeoutInSeconds) {
+        this(driver, Duration.ofSeconds(10));
+    }
+
+        @DataProvider(name = "Authentication")
+        public static Object[][] credentials() {
+            return new Object[][] { { "admin", "password" }};
+        }
+
+        @Test (dataProvider = "Authentication")
+        public void loginTestCase(String username, String password) {
+            //Find username and pasword fields
+            WebElement usernameField = driver.findElement(By.id("username"));
+            WebElement passwordField = driver.findElement(By.id("password"));
+
+            //Enter values
+            usernameField.sendKeys(username);
+            passwordField.sendKeys(password);
+
+            //Click Log in
+            driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+            //Assert Message
+            String loginMessage = driver.findElement(By.id("action-confirmation")).getText();
+            Assert.assertEquals(loginMessage, "Welcome Back, admin");
+        }
+
+        @AfterClass
+        public void afterClass() {
+            //Close browser
+            driver.close();
+        }
+
+    }
